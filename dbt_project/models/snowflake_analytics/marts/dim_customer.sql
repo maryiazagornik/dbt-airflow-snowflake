5d604da2b03aefc00fe2e0912f3c994ad8b3b327
Maryia Zagornikova <maryia.zagornikova@innowise.com>
2025-12-12 13:48:22 +0300

    feat: implement PIT table for performance optimization

--- dbt_project/models/snowflake_analytics/marts/dim_customer.sql
+++ dbt_project/models/snowflake_analytics/marts/dim_customer.sql
@@ -1,28 +1,35 @@
 {{ config(materialized='table') }}
 
 WITH pit AS (
+    SELECT * FROM {{ ref('pit_customer') }}
+),
+hub AS (
     SELECT * FROM {{ ref('hub_customer') }}
 ),
 sat_invar AS (
     SELECT * FROM {{ ref('sat_customer_invar') }}
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY CUSTOMER_PK ORDER BY LOAD_DATE DESC) = 1
 ),
 sat_var AS (
     SELECT * FROM {{ ref('sat_customer_var') }}
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY CUSTOMER_PK ORDER BY LOAD_DATE DESC) = 1
 ),
 sat_biz AS (
     SELECT * FROM {{ ref('sat_customer_business') }}
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY CUSTOMER_PK ORDER BY LOAD_DATE DESC) = 1
 )
 
 SELECT
     p.CUSTOMER_PK,
-    p.CUSTOMER_ID,
+    h.CUSTOMER_ID,
     i.CUSTOMER_NAME AS NAME,
     v.CUSTOMER_ADDRESS AS ADDRESS,
     b.MARKETING_GROUP
 FROM pit p
-LEFT JOIN sat_invar i ON p.CUSTOMER_PK = i.CUSTOMER_PK
-LEFT JOIN sat_var v ON p.CUSTOMER_PK = v.CUSTOMER_PK
-LEFT JOIN sat_biz b ON p.CUSTOMER_PK = b.CUSTOMER_PK
+INNER JOIN hub h ON p.CUSTOMER_PK = h.CUSTOMER_PK
+LEFT JOIN sat_invar i 
+    ON p.CUSTOMER_PK = i.CUSTOMER_PK 
+    AND p.SAT_INVAR_LOAD_DATE = i.LOAD_DATE
+LEFT JOIN sat_var v 
+    ON p.CUSTOMER_PK = v.CUSTOMER_PK 
+    AND p.SAT_VAR_LOAD_DATE = v.LOAD_DATE
+LEFT JOIN sat_biz b 
+    ON p.CUSTOMER_PK = b.CUSTOMER_PK 
+    AND p.SAT_BIZ_LOAD_DATE = b.LOAD_DATE
