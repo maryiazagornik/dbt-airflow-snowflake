080d53a65b4b5df5b40d43fb0553a495d25ccfd9
Maryia Zagornikova <maryia.zagornikova@innowise.com>
2025-12-15 10:28:32 +0300

    Fix: move hashing logic to Satellites

--- dbt_project/models/snowflake_analytics/raw_vault/sats/sat_order_details.sql
+++ dbt_project/models/snowflake_analytics/raw_vault/sats/sat_order_details.sql
@@ -1,18 +1,26 @@
 {{ config(
     materialized='incremental',
-    unique_key='ORDER_PK'
+    unique_key=['ORDER_PK', 'LOAD_DATE'],
+    incremental_strategy='merge' 
 ) }}
 
 WITH source_data AS (
     SELECT
-        ORDER_PK,
+        SHA2(COALESCE(TO_VARCHAR(ORDER_ID), ''), 256) AS ORDER_PK,
+        
         ORDER_DATE,
         ORDER_PRIORITY,
         O_CLERK,
         TOTAL_PRICE,
         LOAD_DATE,
         RECORD_SOURCE,
-        HASHDIFF_DETAILS
+
+        SHA2(CONCAT(
+            COALESCE(TO_VARCHAR(ORDER_DATE), ''), '|',
+            COALESCE(TO_VARCHAR(ORDER_PRIORITY), ''), '|',
+            COALESCE(TO_VARCHAR(O_CLERK), '')
+        ), 256) AS HASHDIFF_DETAILS
+
     FROM {{ ref('stg_orders') }}
     {% if is_incremental() %}
         WHERE LOAD_DATE > (SELECT MAX(t.LOAD_DATE) FROM {{ this }} AS t)
@@ -25,5 +33,6 @@ SELECT * FROM source_data AS s
         SELECT 1
         FROM {{ this }} AS t
         WHERE t.ORDER_PK = s.ORDER_PK
+          AND t.HASHDIFF_DETAILS = s.HASHDIFF_DETAILS
     )
 {% endif %}
--- dbt_project/models/snowflake_analytics/raw_vault/sats/sat_order_status.sql
+++ dbt_project/models/snowflake_analytics/raw_vault/sats/sat_order_status.sql
@@ -1,29 +1,35 @@
 {{ config(
     materialized='incremental',
-    unique_key='ORDER_PK'
+    unique_key=['ORDER_PK', 'LOAD_DATE'],
+    incremental_strategy='merge'
 ) }}
 
 WITH source_data AS (
     SELECT
-        ORDER_PK,
+        SHA2(COALESCE(TO_VARCHAR(ORDER_ID), ''), 256) AS ORDER_PK,
+
         ORDER_STATUS,
         TOTAL_PRICE,
         LOAD_DATE,
         RECORD_SOURCE,
-        HASHDIFF_STATUS
+
+        SHA2(CONCAT(
+            COALESCE(TO_VARCHAR(ORDER_STATUS), ''), '|',
+            COALESCE(TO_VARCHAR(TOTAL_PRICE), '')
+        ), 256) AS HASHDIFF_STATUS
+
     FROM {{ ref('stg_orders') }}
     {% if is_incremental() %}
         WHERE LOAD_DATE > (SELECT MAX(t.LOAD_DATE) FROM {{ this }} AS t)
     {% endif %}
 )
 
-SELECT * FROM source_data
+SELECT * FROM source_data AS s
 {% if is_incremental() %}
     WHERE NOT EXISTS (
         SELECT 1
         FROM {{ this }} AS t
-        WHERE
-            t.ORDER_PK = source_data.ORDER_PK
-            AND t.HASHDIFF_STATUS = source_data.HASHDIFF_STATUS
+        WHERE t.ORDER_PK = s.ORDER_PK
+          AND t.HASHDIFF_STATUS = s.HASHDIFF_STATUS
     )
 {% endif %}
