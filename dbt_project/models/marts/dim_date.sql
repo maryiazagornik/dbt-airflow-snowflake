ea851e5cc8285699ab92827391a7141174d43815
Maryia Zagornikova <maryia.zagornikova@innowise.com>
2025-12-15 13:01:58 +0300

    Fix date range and link hashes

--- dbt_project/models/marts/dim_date.sql
+++ dbt_project/models/marts/dim_date.sql
@@ -1,8 +1,17 @@
 {{ config(materialized='table') }}
 
-{{ dbt_utils.date_spine(
-    datepart="day",
-    start_date="cast('1990-01-01' as date)",
-    end_date="cast('2000-01-01' as date)"
-   )
-}}
+WITH date_spine AS (
+  {{ dbt_utils.date_spine(
+      datepart="day",
+      start_date="cast('1990-01-01' as date)",
+      end_date="cast('2000-01-01' as date)"
+     )
+  }}
+)
+SELECT
+    DATE_DAY,
+    EXTRACT(YEAR FROM DATE_DAY) AS YEAR,
+    EXTRACT(MONTH FROM DATE_DAY) AS MONTH,
+    EXTRACT(QUARTER FROM DATE_DAY) AS QUARTER,
+    TO_CHAR(DATE_DAY, 'DAY') AS DAY_OF_WEEK
+FROM date_spine
--- dbt_project/models/raw_vault/links/link_customer_order.sql
+++ dbt_project/models/raw_vault/links/link_customer_order.sql
@@ -1,17 +1,9 @@
-{{ config(materialized='incremental', unique_key='LINK_PK') }}
+{{ config(materialized='incremental', unique_key='LINK_CUSTOMER_ORDER_PK') }}
 
-WITH source AS (
-    SELECT
-        MD5(CAST(O_ORDERKEY AS VARCHAR) || CAST(O_CUSTKEY AS VARCHAR)) AS LINK_PK,
-        MD5(CAST(O_ORDERKEY AS VARCHAR)) AS ORDER_PK,
-        MD5(CAST(O_CUSTKEY AS VARCHAR)) AS CUSTOMER_PK,
-        O_ORDERDATE AS LOAD_DATE,
-        'SNOWFLAKE_SAMPLE' AS RECORD_SOURCE
-    FROM {{ source('tpch', 'orders') }}
-    WHERE O_ORDERDATE <= DATE('1992-01-31')
-)
-
-SELECT * FROM source
-{% if is_incremental() %}
-    WHERE LINK_PK NOT IN (SELECT t.LINK_PK FROM {{ this }} AS t)
-{% endif %}
+SELECT DISTINCT
+    {{ dbt_utils.generate_surrogate_key(['ORDER_PK', 'CUSTOMER_PK']) }} as LINK_CUSTOMER_ORDER_PK,
+    ORDER_PK,
+    CUSTOMER_PK,
+    LOAD_DATE,
+    RECORD_SOURCE
+FROM {{ ref('stg_orders') }}
