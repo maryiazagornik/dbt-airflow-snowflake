d8e887ec284126c1bd79b9847f32383be40e9bd1
Maryia Zagornikova <maryia.zagornikova@innowise.com>
2025-12-15 14:09:57 +0300

    Fix sat_order_details to use MD5

--- dbt_project/models/raw_vault/sats/sat_order_details.sql
+++ dbt_project/models/raw_vault/sats/sat_order_details.sql
@@ -1,38 +1,20 @@
-{{ config(
-    materialized='incremental',
-    unique_key=['ORDER_PK', 'LOAD_DATE'],
-    incremental_strategy='merge' 
-) }}
+{{ config(materialized='incremental', unique_key='ORDER_PK') }}
 
-WITH source_data AS (
-    SELECT
-        SHA2(COALESCE(TO_VARCHAR(ORDER_ID), ''), 256) AS ORDER_PK,
-        
-        ORDER_DATE,
-        ORDER_PRIORITY,
-        O_CLERK,
-        TOTAL_PRICE,
-        LOAD_DATE,
-        RECORD_SOURCE,
+SELECT DISTINCT
+    ORDER_PK,
+    
+    ORDER_DATE,
+    TOTAL_PRICE,
+    ORDER_PRIORITY,
+    O_CLERK as CLERK_NAME,
+    SHIP_PRIORITY,
+    ORDER_COMMENT,
+    
+    LOAD_DATE,
+    RECORD_SOURCE,
+    {{ dbt_utils.generate_surrogate_key(['ORDER_DATE', 'TOTAL_PRICE', 'ORDER_PRIORITY']) }} as HASHDIFF
 
-        SHA2(CONCAT(
-            COALESCE(TO_VARCHAR(ORDER_DATE), ''), '|',
-            COALESCE(TO_VARCHAR(ORDER_PRIORITY), ''), '|',
-            COALESCE(TO_VARCHAR(O_CLERK), '')
-        ), 256) AS HASHDIFF_DETAILS
-
-    FROM {{ ref('stg_orders') }}
-    {% if is_incremental() %}
-        WHERE LOAD_DATE > (SELECT MAX(t.LOAD_DATE) FROM {{ this }} AS t)
-    {% endif %}
-)
-
-SELECT * FROM source_data AS s
+FROM {{ ref('stg_orders') }}
 {% if is_incremental() %}
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM {{ this }} AS t
-        WHERE t.ORDER_PK = s.ORDER_PK
-          AND t.HASHDIFF_DETAILS = s.HASHDIFF_DETAILS
-    )
+    WHERE LOAD_DATE > (SELECT MAX(LOAD_DATE) FROM {{ this }})
 {% endif %}
